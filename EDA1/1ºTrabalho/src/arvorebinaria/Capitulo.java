8e96103a7b446db87e29031be2adf790e6a69d67
Nuno Lopes <nun0lopes@hotmail.com>
2016-08-10 22:59:15 +0100

    Mais umas pequenas alterações no coding style

--- EDA1/1ºTrabalho/src/arvorebinaria/Capitulo.java
+++ EDA1/1ºTrabalho/src/arvorebinaria/Capitulo.java
@@ -18,21 +18,28 @@ public class Capitulo implements Comparable<Capitulo>{
 	public String toString() {
 		String a = "";
 		
-        a += nome + ": "; // "Serie: "
-    	
-        if (inicio == fim) {
-    		if (inicio != 0) {
-    			a += inicio ; //"Serie: 2"
-    		}
-    	}
+		a += nome + ": "; // "Serie: "
+		
 
-    	else {
-    		a += inicio + "-" + fim + " "; //"Serie: 2-4"
-    	}
 
-    	if (ocorrencias != null) { // Se tiver ocorrencias
-            a += printOcorrencias();//"Serie: 4,5,6,7"
-    	}
+
+
+
+
+		
+		if (inicio == fim) {
+			if (inicio != 0) {
+				a += inicio ; //"Serie: 2"
+			}
+		}
+
+		else {
+			a += inicio + "-" + fim + " "; //"Serie: 2-4"
+		}
+
+		if (ocorrencias != null) { // Se tiver ocorrencias
+			a += printOcorrencias();//"Serie: 4,5,6,7"
+		}
 
 		if (subcapitulos != null) {
 			a += printSubcapitulos(subcapitulos.raiz);
@@ -43,60 +50,60 @@ public class Capitulo implements Comparable<Capitulo>{
 	
 	private String printSubcapitulos(NoABP<Capitulo> no) {
 		if (no.getElemento() == null) { 
-    		return "";
-    	}
-    	
-        String a = "";
-    	
-        a += printSubcapitulos(no.getEsq());
-    	a += "\n";
-    	a +=  "         " + no.getElemento().nome + ": ";
-    	
-        if (no.getElemento().inicio == no.getElemento().fim) {
-    		if (no.getElemento().inicio != 0) {
-    			a += no.getElemento().inicio ;	
-    		}
-    	}
-    	else {
-    		a += no.getElemento().inicio + "-" + fim + " ";
-    	}
-    	
-        if (no.getElemento().ocorrencias != null) {
-	    	a += no.getElemento().printOcorrencias();
-    	}
-    	
-        a += printSubcapitulos(no.getDir());
-    	
-        return a;
+			return "";
+		}
+		
+		String a = "";
+		
+		a += printSubcapitulos(no.getEsq());
+		a += "\n";
+		a +=  "         " + no.getElemento().nome + ": ";
+		
+		if (no.getElemento().inicio == no.getElemento().fim) {
+			if (no.getElemento().inicio != 0) {
+				a += no.getElemento().inicio ;	
+			}
+		}
+		else {
+			a += no.getElemento().inicio + "-" + fim + " ";
+		}
+		
+		if (no.getElemento().ocorrencias != null) {
+			a += no.getElemento().printOcorrencias();
+		}
+		
+		a += printSubcapitulos(no.getDir());
+		
+		return a;
 	}
 	
 	public String printOcorrencias() {
-    	return printOcorrencias(ocorrencias.raiz);
-    }
-     
-    private String printOcorrencias(NoABP<Integer> no) {
-     	String a = "";
+		return printOcorrencias(ocorrencias.raiz);
+	}
+	 
+	private String printOcorrencias(NoABP<Integer> no) {
+		String a = "";
 
-     	if (no.getElemento() == null) { 
-     		return "";
-     	}
-   	    
-        if (no.getEsq().getElemento() != null) {
-     		a += printOcorrencias(no.getEsq());
-     	}
-        
-        if (!ocorrencias.raiz.getElemento().equals(no.getElemento())) {
-     		a += ", ";
-     	}
-     	
-        a += no.getElemento();
-     	
-     	if (no.getDir().getElemento() != null) {
-     		a += printOcorrencias(no.getDir());
-     	}
-     	
-     	return a;
-    }
+		if (no.getElemento() == null) { 
+			return "";
+		}
+		
+		if (no.getEsq().getElemento() != null) {
+			a += printOcorrencias(no.getEsq());
+		}
+		
+		if (!ocorrencias.raiz.getElemento().equals(no.getElemento())) {
+			a += ", ";
+		}
+		
+		a += no.getElemento();
+		
+		if (no.getDir().getElemento() != null) {
+			a += printOcorrencias(no.getDir());
+		}
+		
+		return a;
+	}
 
 	public void setInicio(int inicio) {
 		this.inicio = inicio;
